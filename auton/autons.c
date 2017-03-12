20e0f52dc6456722a97e38c19b9e617707af7744
shatayu <shatayu@outlook.com>
2017-03-11 21:44:14 -0800

    Added gyroCorrect, planned programming skills

--- auton/autons.c
+++ auton/autons.c
@@ -1,12 +1,12 @@
 /*
-- first L to get 3 center back stars (leaves in front of left fence facing backwards)
+- first starL to get 3 center back stars (leaves in front of left fence facing backwards)
 - run preloadRun 3 times (leaves in front of left fence facing backwards)
-- centerFront dumps 3 center front stars and front right  star (leaves in front of center fence facing backwards)
-- centerCubeDump dumps center cube (leaves in front of center fence facing backwards)
-- sideCube gets back right star and cube, dumps both
+- centerStars gets 3 center front stars and front right star, dumps (leaves in front of center fence facing backwards)
+- centerCube dumps cube (leaves in front of center fence facing backwards)
+- rightCube gets back right star and right cube, dumps over right side fence
 */
 
-void L () {
+void starL () {
 	// deploy
 
 	move(1000, FORWARD); // grab 3 center back stars
@@ -27,5 +27,6 @@ void L () {
 	startTask(liftLaunch);
 	move(300, BACKWARD);
 	stopTask(velocityCloseClaw);
+
 	startTask(openClaw);
 }
